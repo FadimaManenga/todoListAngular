9ca325d0e8a610775e2e8c0b5b04c1c309d19682
Fadima <manenga@gmail.com>
2019-10-01 23:16:50 +0200

    soubscription au service todoService

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from 'src/app/services/todo.service';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
+  providers:[TodoService] 
 })
 export class TodoComponent implements OnInit {
 
-  constructor() { }
+  // Création et initialisation du conteneur liste des tâches
+  toDoListArray: any[];
+
+  constructor(private toDoService: TodoService) { }
 
   ngOnInit() {
+
+// soubscription au service ToDoService
+this.toDoService.getToDolist().snapshotChanges()
+.subscribe(item => {
+  this.toDoListArray = [];
+  item.forEach(element => {
+    var x =  element.payload.toJSON();
+    x["$key"] = element.key;
+    this.toDoListArray.push(x);
+  })
+
+    // sort array isChecked false -> true
+    this.toDoListArray.sort((a,b) => {
+      return a.isChecked - b.isChecked;
+      })
+    });
   }
 
+  // ajout de tâche
+  onAdd(itemTitle) {
+    this.toDoService.addTitle(itemTitle.value);
+    itemTitle.value = null;
+  }
+
+  // tâche validé
+  alterCheck($key: string,isChecked){
+    this.toDoService.checkOrUnCheckTitle($key,!isChecked);
+  }
+
+  // suppression de tâche
+  onDelete($key: string) {
+    this.toDoService.removeTitle($key);
+  }
 }
