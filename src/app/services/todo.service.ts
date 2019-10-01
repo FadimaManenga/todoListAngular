c200b8f1130693b36e0e9ca8e4a5e3bb0f36fd86
Fadima <manenga@gmail.com>
2019-10-01 22:55:15 +0200

    todoService CRUD

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from '@angular/Fire/Database';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
-  constructor() { }
+  toDoList: AngularFireList<any>;
+
+  constructor(private firebasedb: AngularFireDatabase) { }
+
+  // méthode CRUD
+  // Création de ttes les fonctionnalités nécessaire à la gestion des tâches, lié à la BDD firebase :
+
+
+// récup de tâches depuis la BDD : méthode get()
+  getToDolist() {
+    this.toDoList = this.firebasedb.list('titles');
+    return this.toDoList;
+  }
+
+  // insertion de tâche dans la BDD : méthode add()
+  addTitle(title: string) {
+    this.toDoList.push ({
+      title: title,
+      isChecked: false
+    });
+  }
+
+  // MAJ des tâche par la validation : méthode update()
+  checkOrUnCheckTitle($key: string, flag:boolean) {
+    this.toDoList.update($key, { isChecked:flag });
+  }
+  
+
+  // suppression de tâche : méthode remove()
+  removeTitle($key: string) {
+    this.toDoList.remove($key);
+  }
+
 }
+
+
